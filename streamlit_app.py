8187feae0834e5a3357e1c7b56661aff9605ec43
nikhil1home <129803820+nikhil1home@users.noreply.github.com>
2023-04-03 16:45:02 -0700

    fixed type

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,6 @@ streamlit.text("🥑🍞Avacado Toast")
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-my_fruit_list = pands.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 streamlit.dataframe(my_fruit_list)
