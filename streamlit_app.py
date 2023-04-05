f1ac8b16f243823523f86e69c51571c49d76a1f6
nikhil1home <129803820+nikhil1home@users.noreply.github.com>
2023-04-04 18:20:50 -0700

    added second text box

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,13 +2,6 @@ import streamlit
 import pandas
 import snowflake.connector
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
-streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
-
 import requests
 
 streamlit.title("My Parents New Healthy Diner")
@@ -43,3 +36,14 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("select * from fruit_load_list")
+my_data_rows = my_cur.fetchall()
+streamlit.header("The fruit load list contains:")
+streamlit.dataframe(my_data_rows)
+
+
+fruit_choice_second = streamlit.text_input('What fruit would you like to add?','Banana')
+streamlit.write('Thanks for adding ', fruit_choice_second)
